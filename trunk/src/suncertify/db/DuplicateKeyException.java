b05165f6ae9a7c4be00eb6a90cbc4303f4d68a3e
cmeury <cmeury@d467975c-d02c-251c-61b0-3e4d88a160b5>
2010-10-06 21:22:29 +0000

    Adapted basic exceptions to adhere to the assignment

--- trunk/src/suncertify/db/DuplicateKeyException.java
+++ trunk/src/suncertify/db/DuplicateKeyException.java
@@ -1,5 +1,24 @@
 package suncertify.db;
 
+/**
+ * Gets thrown when trying to add a data-set which already exists
+ */
 public class DuplicateKeyException extends Exception {
-
+	
+	private String description;
+	
+	// Sets the description to the original exception message
+	public DuplicateKeyException() {
+		this.description = super.getMessage();
+	}
+	
+	// Stores given string as description
+	public DuplicateKeyException(String description) {
+		this.description = description;
+	}
+	
+	@Override
+	public String getMessage() {
+		return description;
+	}
 }
