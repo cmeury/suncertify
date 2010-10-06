42d105a6f90ed8c15b9858ac52bc2a133314cf06
cmeury <cmeury@d467975c-d02c-251c-61b0-3e4d88a160b5>
2010-10-06 21:46:39 +0000

    Added test source folder and test stubs for the basic DB interface taken from the assignment

---
+++ trunk/srcTest/suncertify/db/RecordNotFoundExceptionTest.java
@@ -0,0 +1,24 @@
+package suncertify.db;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RecordNotFoundExceptionTest {
+
+	@Test
+	public void testRecordNotFoundException() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRecordNotFoundExceptionString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMessage() {
+		fail("Not yet implemented");
+	}
+
+}
