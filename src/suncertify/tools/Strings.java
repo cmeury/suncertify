c5ed117b710cc28b4fa1f7bf1f2321e2795f05fd
cmeury <cmeury@d467975c-d02c-251c-61b0-3e4d88a160b5>
2010-10-10 11:34:57 +0000

    Add commonly used strings

---
+++ src/suncertify/tools/Strings.java
@@ -0,0 +1,19 @@
+package suncertify.tools;
+
+/**
+ * Provides commonly used strings from a centralized location to ease maintenance
+ *
+ */
+public class Strings {
+	private static final String applicationName = "URLyBird Booking System";
+	private static final String mainWindowLabel = "Following is the list of records in the data file:";
+
+	public static String getMainwindowlabel() {
+		return mainWindowLabel;
+	}
+
+	public static String getApplicationName() {
+		return applicationName;
+	}
+	
+}
