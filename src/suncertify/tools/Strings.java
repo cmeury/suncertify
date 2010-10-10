356a8454a9d8cceeb4c49f252a9aa5e36549ee35
cmeury <cmeury@d467975c-d02c-251c-61b0-3e4d88a160b5>
2010-10-10 12:20:41 +0000

    Added list of column names, very crudely

--- src/suncertify/tools/Strings.java
+++ src/suncertify/tools/Strings.java
@@ -7,6 +7,20 @@ package suncertify.tools;
 public class Strings {
 	private static final String applicationName = "URLyBird Booking System";
 	private static final String mainWindowLabel = "Following is the list of records in the data file:";
+	private static final String[] columnNames = {
+			"Deleted",
+			"Name",
+			"City",
+			"Size",
+			"Smoking",
+			"Rate",
+			"Date",
+			"Owner",
+			};
+
+	public static String[] getColumnNames() {
+		return columnNames;
+	}
 
 	public static String getMainwindowlabel() {
 		return mainWindowLabel;
